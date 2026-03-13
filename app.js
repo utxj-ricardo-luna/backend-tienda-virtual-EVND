f7e9315097b017ce826d4794fe6bc3cb9e92483d
Ricardo Luna Santos <ricardo.luna@utxicotepec.edu.mx>
2026-03-13 14:34:57 -0600

    Crear servidor node express

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const http = require('http');
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, res)=> res.status(200).send({
+    message: 'Bienvenido al API de tienda virtual',
+}));
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
+module.exports = app;
